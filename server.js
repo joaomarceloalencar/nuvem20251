ee52c27b663a9ae63e1ae8f59bd0862579dfa00f
joaomarceloalencar <joao.marcelo@ufc.br>
2025-07-11 14:27:36 -0300

    Alterações em sala.

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const pool = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
+    ssl: { rejectUnauthorized: false } 
 });
 
 // Inicializar banco de dados PostgreSQL
